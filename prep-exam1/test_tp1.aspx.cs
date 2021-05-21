4bfde08a5432b25c8454f1cd4fdee5065c933d74
Fahd KORAICHE <koraiche_fahd@emsi-edu.ma>
2021-05-21 19:44:05 +0000

    second commit

---
+++ prep-exam1/test_tp1.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace prep_exam1
+{
+    public partial class test_tp1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
